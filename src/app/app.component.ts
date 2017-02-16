c951217201cfb86058ba87f25ed6980eb907bda4
Geraldine <geraldine@g-angle.co.jp>
2017-02-16 10:43:04 +0800

    Add async oberservables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { HttpService } from './http.service';
 })
 export class AppComponent {
   items: any[] = [];
+  asyncString = this.httpService.getData();
 
   constructor(private httpService: HttpService) {}
 
