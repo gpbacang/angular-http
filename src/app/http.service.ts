2d94bfef256ad13475552ff95d26bca88a1cbcb2
Geraldine <geraldine@g-angle.co.jp>
2017-02-16 16:08:49 +0800

    Add error handling

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/Rx';
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class HttpService {
@@ -19,11 +20,17 @@ export class HttpService {
     return this.http.post('https://angular-course-b46c6.firebaseio.com/data.json', body, {
       headers: headers
     })
-      .map((data: Response) => data.json());
+      .map((data: Response) => data.json())
+      .catch(this.handleError);
   }
 
   getOwnData() {
     return this.http.get('https://angular-course-b46c6.firebaseio.com/data.json')
       .map((response: Response) => response.json());
+  }
+
+  private handleError(error: any) {
+    console.log(error);
+    return Observable.throw(error.json());
   }
 }
